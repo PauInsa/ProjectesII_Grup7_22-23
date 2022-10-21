48c7b7cbb4b3cb3f46b6c35639dc891496cad542
Pau Insa <pau.insa@enti.cat>
2022-10-21 17:23:18 +0200

    Project.Upload

---
+++ Assets/Scripts/Mirilla.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+public class Mirilla : MonoBehaviour
+{
+    public Transform personaje;
+    public int size = 1;
+    public float factor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (size <= 0) return;
+
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 vectorPjMouse = mouseWorldPosition - personaje.position;
+        Vector3 xyVector = new Vector3(vectorPjMouse.x, vectorPjMouse.y, 0.0f);
+        float vectorSize = xyVector.magnitude;
+        if (vectorSize > size)
+        {
+            xyVector.Normalize();
+            xyVector *= size;
+            vectorSize = size;
+        }
+
+        factor = Mathf.Clamp01(vectorSize / size);
+        transform.position = xyVector + personaje.position;
+    }
+}
