e9797b7cdfb22a5a0314689d4fd5e43d8e725cdc
Pau Insa <pau.insa@enti.cat>
2022-11-16 12:04:11 +0100

    No reload

--- Assets/Scripts/Gun/Shoot.cs
+++ Assets/Scripts/Gun/Shoot.cs
@@ -24,24 +24,16 @@ public class Shoot : MonoBehaviour
     public float reloadTime;
     float deltaTimeFire;
 
-    public int ammo;
-
-    bool reloading;
-    float deltaTimeReload;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        ammo = 10;
-        reloading = false;
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        amo_text.text = "Ammo: " + ammo;
-
         Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         direction = mouseWorldPosition - (Vector2)gun.position;
         gun.transform.right = direction;
@@ -49,42 +41,24 @@ public class Shoot : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             //Shoot
-            if (ammo > 0 && reloading == false)
+            if (Time.time > deltaTimeFire)
             {
-                if (Time.time > deltaTimeFire)
-                {
-                    recoil();
-                    if (movScript.together == true)
-                        movScript.recoil();
+                recoil();
+                if (movScript.together == true)
+                    movScript.recoil();
 
-                    deltaTimeFire = Time.time + 1 / fireRate;
-                    GameObject goBullet = Instantiate(bullet, gun.position, shootPoint.rotation);
-                    goBullet.transform.right = direction;
-                    goBullet.GetComponent<Rigidbody2D>().AddForce(goBullet.transform.right * bulletSpd);
-
-                    ammo--;
-                }
+                deltaTimeFire = Time.time + 1 / fireRate;
+                GameObject goBullet = Instantiate(bullet, gun.position, shootPoint.rotation);
+                goBullet.transform.right = direction;
+                goBullet.GetComponent<Rigidbody2D>().AddForce(goBullet.transform.right * bulletSpd);
             }
         }
-        else if (Input.GetMouseButtonDown(1))
-            reload();
 
         //Sprite rotation
         if (mouseWorldPosition.x >= gun.position.x)
             gunRender.flipY = false;
         else
             gunRender.flipY = true;
-
-        //Reload
-        if (reloading == true)
-        {
-            reloadSound.Play();
-            if (Time.time > reloadTime + deltaTimeReload)
-            {
-                ammo = 10;
-                reloading = false;
-            }
-        }
     }
     void recoil()
     {
@@ -92,14 +66,6 @@ public class Shoot : MonoBehaviour
         Vector2 xyVector = new Vector2(direction.x, direction.y);
         xyVector.Normalize();
         rb.AddForce(xyVector * -1500.0f);
-    }
-    void reload()
-    {
-        if (reloading == false && ammo !=10)
-        {
-            reloading = true;
-            deltaTimeReload = Time.time;
-        }
     }
 }
 
