ce5db7a2f247aa895cbbe62d6a08e0ebbb860117
Joel Olmedo Noguera <joel.olmedo@enti.cat>
2023-01-07 12:23:09 +0100

    enemigo feedback

--- Assets/Scripts/Enemy script/Enemys.cs
+++ Assets/Scripts/Enemy script/Enemys.cs
@@ -6,6 +6,7 @@ public class Enemys : MonoBehaviour
 {
     public int lifes;
     public Transform enemy;
+    public ParticleSystem particle;
 
     private void Start()
     {
@@ -14,9 +15,19 @@ public class Enemys : MonoBehaviour
 
     void Update()
     {
-        if (lifes < 1)
+        if (lifes <= 0)
         {
             
+            //Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (lifes == 0)
+        {
+            Corpse();
+            lifes = -1;
             //Destroy(gameObject);
         }
     }
@@ -33,5 +44,18 @@ public class Enemys : MonoBehaviour
     {
         //reciveDamage.Play();
         lifes -= damage;
+        
+    }
+
+    private void Corpse()
+    {
+        particle.Play();
+
+        Vector2 impulse = new Vector2(15.0f, 45.0f);
+        impulse.Normalize();
+        GetComponent<Rigidbody2D>().AddForce(impulse * 4, ForceMode2D.Impulse);
+
+        Vector3 asdd = new Vector3(0, 0, -90f);
+        GetComponent<Transform>().Rotate(asdd);
     }
 }
